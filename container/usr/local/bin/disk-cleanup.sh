d6f585c07ba3e2bd256813e0d687843d1fd9a4b0
renedis <renedis@hotmail.com>
2024-01-05 00:11:21 +0100

    Added echo log output

--- container/usr/local/bin/disk-cleanup.sh
+++ container/usr/local/bin/disk-cleanup.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 set -euo pipefail
-IFS=$'\n\t'
 
 if [[ -n ${VERBOSE:-} ]]; then
-    set -x
+  set -x
 fi
 
 TARGET_DIR="${TARGET_DIR:-}"
 MAX_AGE="${MAX_AGE:-180}"
 if [[ -z "${TARGET_DIR}" ]]; then
-    echo "Unknown TARGET_DIR"
-    exit 1
+  echo "Unknown TARGET_DIR"
+  exit 1
 fi
 
 if [[ ! -d "${TARGET_DIR}" ]]; then
-    echo "Destination directory not found"
-    exit 1
+  echo "Destination directory not found"
+  exit 1
 fi
 
 TARGET_DIR_PATH="${TARGET_DIR/%\//}"
 FIND_ARGS=(
-    "${TARGET_DIR_PATH}"
-    -maxdepth 1
-    -iname 'daily.*'
-    -mtime +${MAX_AGE}
-    -type d
+  "${TARGET_DIR_PATH}"
+  -maxdepth 1
+  -iname 'daily.*'
+  -mtime "+${MAX_AGE}"
+  -type d
 )
 
 # If there's no files to delete, that's ok
-if ! ( find "${FIND_ARGS[@]}" \
-    | grep -v incomplete \
-    | grep -q -v "$(basename "$(readlink "${TARGET_DIR_PATH}/daily.current")")" ); then
-    exit 0
+if ! (find "${FIND_ARGS[@]}" \
+  | grep -v incomplete \
+  | grep -q -v "$(basename "$(readlink "${TARGET_DIR_PATH}/daily.current")")"); then
+  echo "No files to delete older then $MAX_AGE days."
+  exit 0
 fi
 
 # print found files for logging
 find "${FIND_ARGS[@]}" \
-    | grep -v incomplete \
-    | grep -v "$(basename "$(readlink "${TARGET_DIR_PATH}/daily.current")")" \
-    | sort
+  | grep -v incomplete \
+  | grep -v "$(basename "$(readlink "${TARGET_DIR_PATH}/daily.current")")" \
+  | sort
 
 # 1. Find all old backups
 # 2. Exclude the current in progress folder
 # 3. Exclude the current backup
 # 4. remove found backups
 find "${FIND_ARGS[@]}" \
-    | grep -v incomplete \
-    | grep -v "$(basename "$(readlink "${TARGET_DIR_PATH}/daily.current")")" \
-    | sort \
-    | xargs -t -r rm -rf
+  | grep -v incomplete \
+  | grep -v "$(basename "$(readlink "${TARGET_DIR_PATH}/daily.current")")" \
+  | sort \
+  | xargs -t -r rm -rf
+  echo "Files found to delete older then $MAX_AGE days. They are now deleted."
