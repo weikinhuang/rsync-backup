d2c08c1cd2a37d50995d9f85212ea4e9543d3abe
renedis <renedis@hotmail.com>
2024-01-06 14:16:14 +0100

    Add stats and progress for rsync

--- container/usr/local/bin/disk-backup.sh
+++ container/usr/local/bin/disk-backup.sh
@@ -101,6 +101,8 @@ RSYNC_ARGS=(
   --delete
   --ignore-errors
   --verbose
+  --stats
+  --progress
   -F # --filter='dir-merge /.rsync-filter' repeated: --filter='- .rsync-filter'
   --rsh="ssh -p ${SSH_PORT:-22} ${SSH_LOGGING_LEVEL} -o ConnectTimeout=${SSH_CONNECT_TIMEOUT:-5} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${SSH_KEYFILE} ${SSH_OPTIONS:-}"
   --link-dest="${CURRENT_TARGET_DIR}/"
